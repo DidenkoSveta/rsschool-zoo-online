ba23605f71d6a5553c5c08c3f655e98760c2733c
DidenkoSveta <106669577+DidenkoSveta@users.noreply.github.com>
2022-10-04 01:11:47 +0400

    added adaptive layout of the site

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,30 @@
-// @media screen and (max-width: 320px)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @media screen and (max-width: 320px) {}
+// @media screen and (max-width: 640px) {}
+// @media screen and (max-width: 1000px) {}
+
+@media screen and (max-width: 1000px) {
+	.pick-and-feed	{}
+}
+
+@media screen and (max-width: 640px) {
+	.pick-and-feed {}
+}
+
+@media screen and (max-width: 320px) {
+	.pick-and-feed {}
+}
